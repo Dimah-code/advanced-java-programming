40c1936b906e1d2d749586455b2b0195337c353c
dimah <hammyghareghani@gmail.com>
2025-07-20 09:58:51 +0330

    feat: add a println in main method to Example_4_8 class

--- src/Unit_4/Examples/Example_8/Example_4_8.java
+++ src/Unit_4/Examples/Example_8/Example_4_8.java
@@ -5,6 +5,7 @@ public class Example_4_8 {
         employee emp1 = new employee(4411, 100000);
         employee emp2 = new employee(4412, 120000);
         emp1.display();
+        System.out.println();
         emp2.display();
     }
 }
