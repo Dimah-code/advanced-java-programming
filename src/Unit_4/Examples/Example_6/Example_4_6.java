f1deae4ffb0d3fe7f17a0cb662da0e1b556ce62f
dimah <hammyghareghani@gmail.com>
2025-07-20 09:03:11 +0330

    feat: rename object names in Example_4_6 class in Unit_4.Examples.Example_6 package

--- src/Unit_4/Examples/Example_6/Example_4_6.java
+++ src/Unit_4/Examples/Example_6/Example_4_6.java
@@ -1,9 +1,9 @@
 package Unit_4.Examples.Example_6;
 public class Example_4_6 {
     public static void main(String[] args) {
-        Date1 d1 = new Date1();
-        Date1 d2 = new Date1(2011, 15, 2);
-        d1.showDate();
-        d2.showDate();
+        Date date = new Date();
+        Date date1 = new Date(2011, 15, 2);
+        date.showDate();
+        date1.showDate();
     }
 }
