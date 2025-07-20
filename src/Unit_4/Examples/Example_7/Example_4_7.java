cc2e04ed65eca97d6cf87b63854940ff1d8aa4d1
dimah <hammyghareghani@gmail.com>
2025-07-20 09:21:48 +0330

    feat: rename object names in Example_4_7 class in Unit_4.Examples.Example_7 package

--- src/Unit_4/Examples/Example_7/Example_4_7.java
+++ src/Unit_4/Examples/Example_7/Example_4_7.java
@@ -3,22 +3,24 @@ public class Example_4_7 {
     public static void main(String[] args) {
         staticTest obj1 = new staticTest();
         staticTest obj2 = new staticTest();
-        System.out.println("line 7: obj1: " + obj1.getcount());
-        System.out.println("line 8: obj1: " + obj2.getcount());
-        System.out.println("++ with class");
+        System.out.println("> objects first print: ");
+        System.out.println("> object1: " + obj1);
+        System.out.println("> object1: " + obj2);
+        System.out.println(">> Increment count using the class");
         staticTest.incrementCount();
-        System.out.println("obj1 : " + obj1.getcount());
-        System.out.println("obj2 : " + obj2.getcount());
-        System.out.println("Increment count using obj1");
+        System.out.println(">> object1: " + obj1);
+        System.out.println(">> object2: " + obj2);
+        System.out.println(">>> Increment count using obj1");
         obj1.incrementCount();
-        System.out.println("obj1: " + obj1.getcount());
-        System.out.println("obj2: " + obj2.getcount());
+        System.out.println(">>> object1: " + obj1);
+        System.out.println(">>> object2: " + obj2);
+        System.out.println(">>>> Increment count using obj2");
         obj2.incrementCount();
-        System.out.println("obj1: " + obj1.getcount());
-        System.out.println("obj2: " + obj2.getcount());
-        System.out.println("set x to 10");
+        System.out.println(">>>> object1: " + obj1);
+        System.out.println(">>>> object2: " + obj2);
+        System.out.println(">>>>> set x to 10 in object1");
         obj1.setX(10);
-        System.out.println("obj1 " + obj1.getcount());
-        System.out.println("obj2 " + obj2.getcount());
+        System.out.println(">>>>> object1: " + obj1);
+        System.out.println(">>>>> object2: " + obj2);
     }
 }
