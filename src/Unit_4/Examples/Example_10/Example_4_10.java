ca89d57827a8ff09e69669a5a197bde3a5f84d44
dimah <hammyghareghani@gmail.com>
2025-07-20 10:07:05 +0330

    feat: rename object names in Example_4_10

--- src/Unit_4/Examples/Example_10/Example_4_10.java
+++ src/Unit_4/Examples/Example_10/Example_4_10.java
@@ -1,18 +1,18 @@
 package Unit_4.Examples.Example_10;
 public class Example_4_10 {
     public static void main(String[] args) {
-        Change c = new Change();
+        Change change = new Change();
         int a = 100;
         int b = 200;
-        c.setXY(a , b);
+        change.setXY(a , b);
         System.out.println("Before Change");
-        c.show();
-        c = changeMethod(c);
+        change.show();
+        change = changeMethod(change);
         System.out.println("After change");
-        c.show();
+        change.show();
     }
-    public static Change changeMethod(Change c){
-        c.switchNumbers();
-        return c;
+    public static Change changeMethod(Change change){
+        change.switchNumbers();
+        return change;
     }
 }
