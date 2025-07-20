1a18a76c461662f07740259821a3974d13336980
dimah <hammyghareghani@gmail.com>
2025-07-20 08:49:13 +0330

    feat: update main method in Example_4_4

--- src/Unit_4/Examples/Example_4/Example_4_4.java
+++ src/Unit_4/Examples/Example_4/Example_4_4.java
@@ -1,10 +1,10 @@
 package Unit_4.Examples.Example_4;
-import java.util.*;
+
 public class Example_4_4{
     public static void main(String[] args) {
-        Time t1 = new Time(10, 24, 14);
-        Time t2 = new Time(13, 22,33);
-        t1.showTime();
-        t2.showTime();
+        Time time = new Time(10, 24, 14);
+        Time time1 = new Time(13, 22,33);
+        time.showTime();
+        time1.showTime();
     }
 }
