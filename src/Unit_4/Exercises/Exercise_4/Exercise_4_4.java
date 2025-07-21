b8c9af89cb58e0b24b2a03a01bbfa7828243ed26
dimah <hammyghareghani@gmail.com>
2025-07-21 13:37:15 +0330

    feat: rename variable names in Exercise_4_4 class

--- src/Unit_4/Exercises/Exercise_4/Exercise_4_4.java
+++ src/Unit_4/Exercises/Exercise_4/Exercise_4_4.java
@@ -1,9 +1,9 @@
 package Unit_4.Exercises.Exercise_4;
 import java.util.*;
-public class Exercise_4_4 {
+public class    Exercise_4_4 {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        int sum = 0, square, cube, input, sumSq = 0, sumCube = 0;
+        int sum = 0, square, cube, input, sumSquare = 0, sumCube = 0;
 
         do{
             System.out.println("Enter numbers (cancel with 0");
@@ -11,11 +11,11 @@ public class Exercise_4_4 {
             square = input * input;
             cube  = input * input * input;
             sum += input;
-            sumSq += square;
+            sumSquare += square;
             sumCube += cube;
         }while(input != 0);
         System.out.println("sum of numbers: " + sum);
-        System.out.println("sum of square of numbers: " + sumSq);
+        System.out.println("sum of square of numbers: " + sumSquare);
         System.out.println("sum of cube of numbers: " + sumCube);
     }
 }
