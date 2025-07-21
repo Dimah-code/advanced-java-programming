43b7f29838f21e22ef39d401a7c8719448ae9382
dimah <hammyghareghani@gmail.com>
2025-07-21 13:44:48 +0330

    feat: update readScore method in Student class

--- src/Unit_4/Exercises/Exercise_5/Student.java
+++ src/Unit_4/Exercises/Exercise_5/Student.java
@@ -1,13 +1,13 @@
 package Unit_4.Exercises.Exercise_5;
 import java.util.*;
-public class student {
+public class Student {
     static Scanner in  = new Scanner(System.in);
     private double score;
-    private double avg;
+    private double average;
     private double sum;
     private int counter;
-    public void readScore(){
-        System.out.println("Enter Grade Course: ");
+    public void readScore(int index){
+        System.out.printf("Enter Grade Course %d: ", (index + 1));
         score = in.nextDouble();
         counter++;
     }
@@ -15,10 +15,10 @@ public class student {
         sum += score;
     }
     public void getAvg(){
-        avg = sum / counter;
+        average = sum / counter;
     }
     public void display(){
-        System.out.printf("sum of scores: %.2f\n" , sum);
-        System.out.printf("Student's Average: %.2f", avg);
+        System.out.printf("Sum of scores: %.2f\n" , sum);
+        System.out.printf("Student's Average: %.2f", average);
     }
 }
