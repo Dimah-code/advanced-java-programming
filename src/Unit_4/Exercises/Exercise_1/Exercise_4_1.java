1c1e4cd03235957a6cf8190fc4d1eb6d0405ca54
dimah <hammyghareghani@gmail.com>
2025-07-21 11:08:27 +0330

    feat: rename object names in Exercise_4_1 class

--- src/Unit_4/Exercises/Exercise_1/Exercise_4_1.java
+++ src/Unit_4/Exercises/Exercise_1/Exercise_4_1.java
@@ -2,9 +2,9 @@ package Unit_4.Exercises.Exercise_1;
 public class Exercise_4_1 {
 
     public static void main(String[] args) {
-        rectangle rec = new rectangle();
-        rec.readSides();
-        rec.calculate();
-        rec.display();
+        rectangle rectangle = new rectangle();
+        rectangle.readSides();
+        rectangle.calculate();
+        rectangle.display();
     }
 }
