f558e5a90f03fd7fd7ac218bd95c8e479cd436ad
dimah <hammyghareghani@gmail.com>
2025-07-21 11:14:08 +0330

    feat: refactor field names in CheckingAccount class

--- src/Unit_4/Exercises/Exercise_2/CheckingAccount.java
+++ src/Unit_4/Exercises/Exercise_2/CheckingAccount.java
@@ -1,14 +1,14 @@
 package Unit_4.Exercises.Exercise_2;
 public class CheckingAccount {
-    private int accountNO;
+    private int NO;
     private int balance;
     private double minBalance = 200.0;
-    CheckingAccount(int NO, int bal){
-        accountNO = NO;
-        balance = bal;
+    CheckingAccount(int NO, int balance){
+        this.NO = NO;
+        this.balance = balance;
     }
     public void display(){
-        System.out.println("Account NO: " + accountNO);
+        System.out.println("Account NO: " + NO);
         System.out.println("Account Balance: " + balance);
         System.out.println("Minimum Balance: " + minBalance);
     }
