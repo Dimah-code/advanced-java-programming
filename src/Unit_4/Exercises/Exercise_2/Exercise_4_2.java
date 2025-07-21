1ab82bd05d6bec3a2228eb29a58efde299f7a6be
dimah <hammyghareghani@gmail.com>
2025-07-21 11:13:15 +0330

    feat: rename object names in Exercise_4_2 class

--- src/Unit_4/Exercises/Exercise_2/Exercise_4_2.java
+++ src/Unit_4/Exercises/Exercise_2/Exercise_4_2.java
@@ -1,9 +1,9 @@
 package Unit_4.Exercises.Exercise_2;
 public class Exercise_4_2 {
     public static void main(String[] args) {
-        CheckingAccount cA = new CheckingAccount(4021, 100000);
-        CheckingAccount cA1 = new CheckingAccount(4022, 200000);
-        cA.display();
-        cA1.display();
+        CheckingAccount account_1 = new CheckingAccount(4021, 100000);
+        CheckingAccount account_2 = new CheckingAccount(4022, 200000);
+        account_1.display();
+        account_2.display();
     }
 }
