adbdd38ca07ba1c4e61140acc9ef5f5ab6779ab9
dimah <hammyghareghani@gmail.com>
2025-07-22 10:40:47 +0330

    feat: add stringFormatter class to Unit_5.Examples.Example_10

---
+++ src/Unit_5/Examples/Example_10/stringFormatter.java
@@ -0,0 +1,44 @@
+package Unit_5.Examples.Example_10;
+
+public class stringFormatter {
+    public static String leftAdjust(String s, int width) {
+        final int stringLength = s.length();
+        if (stringLength >= width) {
+            return s;
+        } else {
+            return spaces(width - stringLength).insert(0, s).toString();
+        }
+    }
+
+    public static String rightAdjust(String s, int width) {
+        final int stringLength = s.length();
+        if (stringLength >= width) {
+            return s;
+        } else {
+            return spaces(width - stringLength).insert(0, s).toString();
+        }
+    }
+
+    public static String center(String s, int width) {
+        final int stringLength = s.length();
+        if (stringLength >= width) {
+            return s;
+        } else {
+            final int numSpaces = width - stringLength;
+            final int leftSpace = numSpaces / 2, rightSpace = numSpaces - leftSpace;
+            return spaces(leftSpace).append(s).append(spaces(rightSpace)).toString();
+        }
+    }
+
+    private static StringBuffer spaces(int numSpaces) {
+        if (numSpaces <= 0) {
+            return new StringBuffer();
+        } else {
+            StringBuffer spaces = new StringBuffer();
+            for (int i = 0; i < numSpaces; i++) {
+                spaces.append(' ');
+            }
+            return spaces;
+        }
+    }
+}
