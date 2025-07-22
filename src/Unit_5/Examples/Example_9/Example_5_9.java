685094e7a23ed0e2d2bd4a1e67809c5cb233f1c3
dimah <hammyghareghani@gmail.com>
2025-07-22 09:52:21 +0330

    feat: add Example_5_9 class to Unit_5.Examples.Example_9

---
+++ src/Unit_5/Examples/Example_9/Example_5_9.java
@@ -0,0 +1,26 @@
+package Unit_5.Examples.Example_9;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Example_5_9 {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        DateFormat formatter = DateFormat.getDateInstance();
+        System.out.println("Default date: " + formatter.format(now));
+
+        formatter = DateFormat.getDateInstance(DateFormat.SHORT, Locale.);
+        System.out.println("Short (UK): " + formatter.format(now));
+
+        formatter = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        System.out.println("Medium: " + formatter.format(now));
+
+        formatter = DateFormat.getDateInstance(DateFormat.LONG);
+        System.out.println("Long: " + formatter.format(now));
+
+        formatter = DateFormat.getDateInstance(DateFormat.FULL);
+        System.out.println("FULL: " + formatter.format(now));
+    }
+}
