73cdd8d5217c6f8e32262824a33a77b133f9a093
dimah <hammyghareghani@gmail.com>
2025-07-22 09:24:15 +0330

    feat: update main method in Example_5_5 class in Unit_5.Examples.Example_4

--- src/Unit_5/Examples/Example_5/Example_5_5.java
+++ src/Unit_5/Examples/Example_5/Example_5_5.java
@@ -1,11 +1,17 @@
 package Unit_5.Examples.Example_5;
-import java.util.*;
-import java.lang.Character;
+
 public class Example_5_5 {
     public static void main(String[] args) {
-        System.out.println(Character.toUpperCase('a'));
-        System.out.println(Character.isLetter('a'));
-        System.out.println(Character.isLetter('*'));
-        System.out.println();
+        StringBuffer str = new StringBuffer("I hate Mullahs");
+        System.out.println("The person before being sent to intelligence service: " + str);
+        intelligenceService(str);
+        System.out.println("The person after being sent to the intelligence service: " + str);
+    }
+
+    public static void intelligenceService(StringBuffer str) {
+        System.out.println("Person in intelligence service: " + str);
+        System.out.println("A bit of torture");
+        str.append(", But now I love them");
+        System.out.println("The person after a little torture in the intelligence service: " + str);
     }
 }
