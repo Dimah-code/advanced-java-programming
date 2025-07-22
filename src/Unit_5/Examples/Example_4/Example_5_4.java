935e4675b02e66192ae0e9c59cd256a033a72ca0
dimah <hammyghareghani@gmail.com>
2025-07-22 09:07:45 +0330

    feat: refactor my attitude in Example_5_4 class in Unit_5.Examples.Example_4

--- src/Unit_5/Examples/Example_4/Example_5_4.java
+++ src/Unit_5/Examples/Example_4/Example_5_4.java
@@ -2,14 +2,15 @@ package Unit_5.Examples.Example_4;
 import java.util.*;
 public class Example_5_4 {
     public static void main(String[] args) {
-        int str = 10;
-        System.out.println("string before calling method: " + str);
-        switchOrder(str);
-        System.out.println("string after calling method to change it" + str);
+        String str = "I hate Mullahs";
+        System.out.println("The person before being sent to intelligence service: " + str);
+        intelligenceService(str);
+        System.out.println("The person after being sent to the intelligence service: " + str);
     }
-    public static void switchOrder(int strM){
-        System.out.println("str in method before change" + strM);
-        strM = 11;
-        System.out.println("str in method after change" + strM);
+    public static void intelligenceService(String str){
+        System.out.println("Person in intelligence service: " + str);
+        System.out.println("A bit of torture");
+        str = "I love Mullahs";
+        System.out.println("The person after a little torture in the intelligence service: " + str);
     }
 }
