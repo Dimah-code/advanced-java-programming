4b547a8084a417c64f880dfb83e8a39924c35937
dimah <hammyghareghani@gmail.com>
2025-07-22 09:34:52 +0330

    feat: update in main main method in Example_5_8 class to Unit_5.Examples.Example_8

--- src/Unit_5/Examples/Example_8/Example_5_8.java
+++ src/Unit_5/Examples/Example_8/Example_5_8.java
@@ -4,13 +4,12 @@ public class Example_5_8 {
     public static void main(String[] args) {
         NumberFormat formatter = NumberFormat.getNumberInstance();
         formatter.setMaximumFractionDigits(5);
-        System.out.println(formatter.format(Math.PI));
+        System.out.println("Truncated PI = " + formatter.format(Math.PI));
         formatter.setMinimumIntegerDigits(4);
-        System.out.println(formatter.format(Math.PI));
+        System.out.println("Truncated PI = " + formatter.format(Math.PI));
         NumberFormat money = NumberFormat.getCurrencyInstance();
-        System.out.println(money.format((3.53)));
+        System.out.println("Money symbol usage: " + money.format((3.53)));
         NumberFormat percent = NumberFormat.getPercentInstance();
-        System.out.println(percent.format(0.353));
-
+        System.out.println("Percent symbol usage: "+ percent.format(0.353));
     }
 }
