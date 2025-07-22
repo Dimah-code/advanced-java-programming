52b959b23a86d174d81efcbb69bd375a122e50c0
dimah <hammyghareghani@gmail.com>
2025-07-22 09:24:43 +0330

    feat: add Example_5_6 class to Unit_5.Examples.Example_6

---
+++ src/Unit_5/Examples/Example_6/Example_5_6.java
@@ -0,0 +1,14 @@
+package Unit_5.Examples.Example_6;
+
+public class Example_5_6 {
+    public static void main(String[] args) {
+        int x, y;
+        Integer num1, num2;
+        num1 = 8; // Auto box
+        num2 = 16; // Auto box
+        System.out.println("num1 = " + num1 + ", num2 = " + num2);
+        x = 2 * num1 + num2; // Auto unboxing
+        y = num1 + num2; // Auto unboxing
+        System.out.println("x = " + x + ", y = " + y);
+    }
+}
