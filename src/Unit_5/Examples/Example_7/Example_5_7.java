9812fe02331fa5ada508b29ac970b50a37523eac
dimah <hammyghareghani@gmail.com>
2025-07-22 09:29:47 +0330

    feat: add Example_5_7 class to Unit_5.Examples.Example_7

---
+++ src/Unit_5/Examples/Example_7/Example_5_7.java
@@ -0,0 +1,13 @@
+package Unit_5.Examples.Example_7;
+
+import static java.lang.Character.*;
+
+public class Example_5_7 {
+    public static void main(String[] args) {
+        System.out.println("Uppercase ('a') = " + toUpperCase('a'));
+        System.out.println("isLetter('a') = " + isLetter('a'));
+        System.out.println("isLetter('*') = " + isLetter('*'));
+        System.out.println("isUppercase('b') = " + isUpperCase('b'));
+        System.out.println("isSpaceChar(' ') = " + isSpaceChar(' '));
+    }
+}
