d4bb872b60126fdc56e81224811f00f784bd0bf2
dimah <hammyghareghani@gmail.com>
2025-07-29 14:43:59 +0330

    feat: update main method in Example_6_6 class to Unit_6.Examples.Example_6

--- src/Unit_6/Examples/Example_6/Example_6_6.java
+++ src/Unit_6/Examples/Example_6/Example_6_6.java
@@ -3,22 +3,34 @@ import java.util.Scanner;
 
 public class Example_6_6 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 54 ,33, 22, 11,8};
+        Scanner scanner = new Scanner(System.in);
+        int[] nums = new int[10];
+
+        for(int i = 0; i < nums.length; i++){
+            System.out.println("Enter number " + (i + 1) + ": ");
+            nums[i] = scanner.nextInt();
+        }
+
         insertionSort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i] + " , ");
+
+        for (int num : nums) {
+            System.out.print(num + " , ");
         }
     }
     private static void insertionSort(int[] list){
        int temp, loc;
-        for (int i = 1; i < list.length; i++) {
+
+       for (int i = 1; i < list.length; i++) {
             if(list[i] < list[i - 1]){
+
                 temp = list[i];
                 loc = i;
+
                 do{
                     list[loc] = list[loc - 1];
                     loc--;
                 }while(loc > 0 && list[loc - 1] > temp);
+
                 list[loc] = temp;
             }
         }
