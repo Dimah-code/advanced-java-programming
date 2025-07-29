e990438c7cdb2d28ee3d5d82097eb92aa54047fb
dimah <hammyghareghani@gmail.com>
2025-07-29 15:33:32 +0330

    feat: update main method in Example_6_14 class to Unit_6.Examples.Example_14

--- src/Unit_6/Examples/Example_14/TwoDimMethods.java
+++ src/Unit_6/Examples/Example_14/TwoDimMethods.java
@@ -1,33 +1,36 @@
 package Unit_6.Examples.Example_14;
+
 public class TwoDimMethods {
-    public static void printMatrix(int[][] matrix){
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 System.out.printf("%7d", matrix[i][j]);
             }
             System.out.println();
         }
     }
-    public static void sumRows(int[][] matrix){
+
+    public static void sumRows(int[][] matrix) {
         int sum;
-        for(int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             sum = 0;
-            for(int j = 0; j < matrix[i].length; j++){
+            for (int j = 0; j < matrix[i].length; j++) {
                 sum += matrix[i][j];
             }
-            System.out.println("sum[" + i + "]= " + sum);
+            System.out.println("sum[" + i + "] = " + sum);
         }
     }
-    public static void largestInRows(int[][] matrix){
+
+    public static void largestInRows(int[][] matrix) {
         int largest;
-        for (int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             largest = matrix[i][0];
-            for(int j = 0; j < matrix[i].length; j++){
-                if(largest < matrix[i][j]){
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (largest < matrix[i][j]) {
                     largest = matrix[i][j];
                 }
             }
-            System.out.println("Largest number in row[" + i + "]= " + largest);
+            System.out.println("Largest number in row[" + i + "] = " + largest);
         }
     }
 }
