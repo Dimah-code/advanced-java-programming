9d1f94055ffc00cd64b658492864670c2a20ef4d
dimah <hammyghareghani@gmail.com>
2025-07-29 11:04:07 +0330

    feat: add Exercise_5_9 class to Unit_5.Exercises.Exercise_9

---
+++ src/Unit_5/Exercises/Exercise_9/Exercise_5_9.java
@@ -0,0 +1,28 @@
+package Unit_5.Exercises.Exercise_9;
+
+import java.text.ChoiceFormat;
+import java.util.Scanner;
+
+public class Exercise_5_9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0, average, grade;
+        int students;
+
+        System.out.println("How many students do you have: ");
+        students = scanner.nextInt();
+
+        for(int i = 0; i < students; i++){
+            System.out.printf("Enter student[%d] grade:\n", (i + 1));
+            grade = scanner.nextDouble();
+            sum += grade;
+        }
+
+        average = sum / students;
+
+        System.out.printf("Number of students: %d\n", students);
+        System.out.printf("Average of student grades: %6.2f\n", average);
+
+        scanner.close();
+    }
+}
