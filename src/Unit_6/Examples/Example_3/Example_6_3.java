fcedb510df6d62d905c2089f44a3718032669de6
dimah <hammyghareghani@gmail.com>
2025-07-29 14:10:08 +0330

    feat: update main method in Example_6_3 class to Unit_6.Examples.Example_2

--- src/Unit_6/Examples/Example_3/Example_6_3.java
+++ src/Unit_6/Examples/Example_3/Example_6_3.java
@@ -1,19 +1,21 @@
 package Unit_6.Examples.Example_3;
+
 public class Example_6_3 {
     public static void main(String[] args) {
-        int[] nums = {2, 4 , 6, 8};
+        int[] nums = {2, 4, 6, 8};
         System.out.println("Before Calling Method");
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + "\t");
         }
         System.out.println();
-        trans(nums[1] , nums[3]);
+        increment(nums[1], nums[3]);
         System.out.println("After Calling Method");
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + "\t");
         }
     }
-    private static void trans(int a , int b){
+
+    private static void increment(int a, int b) {
         System.out.println("1 and 3 in method: before change: " + a + " " + b);
         a++;
         b++;
