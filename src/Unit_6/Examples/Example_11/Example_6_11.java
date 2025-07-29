edf0ff82a9160da146e051e4d627433375e13803
dimah <hammyghareghani@gmail.com>
2025-07-29 15:16:45 +0330

    feat: update main method in Example_6_11 class to Unit_6.Examples.Example_11

--- src/Unit_6/Examples/Example_11/Example_6_11.java
+++ src/Unit_6/Examples/Example_11/Example_6_11.java
@@ -1,36 +1,42 @@
 package Unit_6.Examples.Example_11;
+
 import java.util.*;
+
 public class Example_6_11 {
     static Scanner in = new Scanner(System.in);
     static String[] master;
+
     public static void main(String[] args) {
         askArraySize();
         enterData();
         System.out.println("Enter name to search in Masters list");
         String name = new String(in.next());
         int found = seqSearch(name);
-        if(found != -1){
+        if (found != -1) {
             System.out.println("Master \"" + master[found] + "\" in array : " + found);
-        }else{
+        } else {
             System.out.println("Master not found!");
         }
     }
-    public static void askArraySize(){
+
+    public static void askArraySize() {
         int num;
-        System.out.println("How many master is in your faclutary: ");
+        System.out.println("How many master is in your university: ");
         num = in.nextInt();
         master = new String[num];
     }
-    public static void enterData(){
-        for(int i = 0; i < master.length; i++){
+
+    public static void enterData() {
+        for (int i = 0; i < master.length; i++) {
             master[i] = new String();
             System.out.println("Enter master " + (i + 1) + " name: ");
             master[i] = in.next();
         }
     }
-    public static int seqSearch(String name){
-        for(int i = 0; i < master.length; i++){
-            if(master[i].equalsIgnoreCase(name)){
+
+    public static int seqSearch(String name) {
+        for (int i = 0; i < master.length; i++) {
+            if (master[i].equalsIgnoreCase(name)) {
                 return i;
             }
         }
