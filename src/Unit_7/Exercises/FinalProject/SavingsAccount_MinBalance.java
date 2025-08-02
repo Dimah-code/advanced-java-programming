a677ac9e904bf17e0f67bcdf08e1671f2ad80ccc
dimah <hammyghareghani@gmail.com>
2025-08-02 11:05:53 +0330

    feat: update Exercise_FinalProject Unit_7.Exercises.FinalProject

---
+++ src/Unit_7/Exercises/FinalProject/SavingsAccount_MinBalance.java
@@ -0,0 +1,23 @@
+package Unit_7.Exercises.FinalProject;
+
+public class SavingsAccount_MinBalance extends BankAccount {
+    private double interestRate;
+    private double minBalance;
+
+    public SavingsAccount_MinBalance(String accNum, double initialBalance, double minBalance, double interestRate) {
+        super(accNum, initialBalance);
+        this.minBalance = minBalance;
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void monthlyUpdate() {
+        if (balance >= minBalance) {
+            double interest = balance * interestRate / 100;
+            balance += interest;
+            System.out.println("Interest added: $" + interest);
+        } else {
+            System.out.println("Below minimum balance, no interest added.");
+        }
+    }
+}
---
+++ src/Unit_7/Exercises/FinalProject/SavingsAccount_NoMinBalance.java
@@ -0,0 +1,17 @@
+package Unit_7.Exercises.FinalProject;
+
+public class SavingsAccount_NoMinBalance extends BankAccount {
+    private double interestRate;
+
+    public SavingsAccount_NoMinBalance(String accNum, double initialBalance, double interestRate) {
+        super(accNum, initialBalance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void monthlyUpdate() {
+        double interest = balance * interestRate / 100;
+        balance += interest;
+        System.out.println("Interest added: $" + interest);
+    }
+}
