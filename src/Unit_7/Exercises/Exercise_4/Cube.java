c955cb9b059e9f69487359173fe30287bf01bb3a
dimah <hammyghareghani@gmail.com>
2025-08-02 13:47:54 +0330

    feat: update Exercise_7_4 Unit_7.Exercises.Exercise_4

--- src/Unit_7/Exercises/Exercise_4/Cube.java
+++ src/Unit_7/Exercises/Exercise_4/Cube.java
@@ -2,15 +2,20 @@ package Unit_7.Exercises.Exercise_4;
 
 public class Cube extends Square {
     private double depth;
-    public Cube(double height, double width, double depth){
+
+    public Cube(double height, double width, double depth) {
         super(height, width);
         this.depth = depth;
         ComputeSurfaceArea();
     }
+
     // getter
-    public double getDepth(){ return depth;}
+    public double getDepth() {
+        return depth;
+    }
+
     @Override
-    public void ComputeSurfaceArea(){
+    public void ComputeSurfaceArea() {
         surfaceArea = 2 * (getHeight() * getWidth() + getHeight() * depth + depth * getWidth());
     }
 
