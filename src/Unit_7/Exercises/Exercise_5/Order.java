901c281ee9f96e751632d9e5dd1acc439404f26b
dimah <hammyghareghani@gmail.com>
2025-08-02 13:49:37 +0330

    feat: update Main Unit_7.Exercises.Exercise_5

--- src/Unit_7/Exercises/Exercise_5/Order.java
+++ src/Unit_7/Exercises/Exercise_5/Order.java
@@ -7,35 +7,60 @@ public class Order {
     private int order;
 
     // Constructors
-    public Order(){
+    public Order() {
         this("No Name", "No Phone", 0, 0);
     }
-    public Order(String customerName, String customerPhone, int price, int order){
+
+    public Order(String customerName, String customerPhone, int price, int order) {
         this.customerName = customerName;
         this.customerPhone = customerPhone;
         this.price = price;
         this.order = order;
         computeFinalPrice();
     }
+
     // Getters
-    public String getCustomerName(){return customerName;}
-    public String getCustomerPhone(){return customerPhone;}
-    public int getPrice(){return price;}
-    public int getOrder(){return order;}
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getOrder() {
+        return order;
+    }
 
     // Setters
-    public void setCustomerName(String customerName){this.customerName = customerName;}
-    public void setCustomerPhone(String customerPhone){this.customerPhone = customerPhone;}
-    public void setPrice(int price){this.price = price;}
-    public void setOrder(int order){this.order = order;}
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
 
     // Compute
 
-    public int computeFinalPrice(){
-         return (order * price);
+    public int computeFinalPrice() {
+        return (order * price);
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("Name: %s, Phone number: %s, Price: %d, Order: %d, Final Price: %d",
                 customerName, customerPhone, price, order, computeFinalPrice());
     }
