d4de53324b89c3528f055de0fc6cc0fec0a30cf5
dimah <hammyghareghani@gmail.com>
2025-08-02 13:35:54 +0330

    feat: update Example_7_2_2 Unit_7.Examples.Example_2_Part_2

--- src/Unit_7/Examples/Example_2/Example_2_Part_2/BaseClass.java
+++ src/Unit_7/Examples/Example_2/Example_2_Part_2/BaseClass.java
@@ -1,21 +1,27 @@
 package Unit_7.Examples.Example_2.Example_2_Part_2;
+
 public class BaseClass {
     private String firstName, lastName;
-    public static void sayHello(){
+
+    public static void sayHello() {
         System.out.println("Hello");
     }
-    public BaseClass(){
+
+    public BaseClass() {
         setName("Emptyfirst", "Emptylast");
     }
-    public BaseClass(String firstName, String lastName){
+
+    public BaseClass(String firstName, String lastName) {
         setName(firstName, lastName);
     }
-    private void setName(String firstName, String lastName){
+
+    private void setName(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
     @Override
-    public String toString(){
-        return  ("Firstname: " + firstName + " Lastname: " + lastName);
+    public String toString() {
+        return ("Firstname: " + firstName + " Lastname: " + lastName);
     }
 }
--- src/Unit_7/Examples/Example_2/Example_2_Part_2/Example_7_2_2.java
+++ src/Unit_7/Examples/Example_2/Example_2_Part_2/Example_7_2_2.java
@@ -1,9 +1,10 @@
 package Unit_7.Examples.Example_2.Example_2_Part_2;
+
 public class Example_7_2_2 {
     public static void main(String[] args) {
         BaseClass base1 = new BaseClass();
         System.out.println(base1);
-        BaseClass base2 = new BaseClass("Hamidreza" , "Ghareghani");
+        BaseClass base2 = new BaseClass("Hamidreza", "Ghareghani");
         System.out.println(base2);
     }
 }
